d4935f31a7f94a82c8078ba6a8aaad31200a6c76
渡邉江平 <kouhei.w.56@gmail.com>
2017-07-29 11:56:56 +0900

    init unicorn01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'infinite-atoll-32229.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: '52.193.11.216'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
